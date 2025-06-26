a7592ce9a6495518992b8e8dc15043c25569ea4f
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-26 11:17:31 +0530

    shell

---
+++ 01-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+person=$1
+person2=$2 
+
+echo "${person} : HI $person2"
+echo "$person2 : HI $person"
+echo "$person : how are you  $person2"
+echo "$person2 : Am fine !    how are you  $person"
