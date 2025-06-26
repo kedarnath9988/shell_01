0837f0df972121e642a0f0e8494de575a19eae9f
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-26 11:26:48 +0530

    shell

--- 01-array.sh
+++ 01-array.sh
@@ -3,4 +3,6 @@
 my_array=("rrr" "tillu" "hello")
 
 echo "the 1st valu is ${my_array}"
-echo "the 1st valu is ${my_array[1]}"
+echo "the 2st valu is ${my_array[1]}"
+echo "the 2st valu is ${my_array[2]}"
+echo "the 1st valu is ${my_array[@]}"
