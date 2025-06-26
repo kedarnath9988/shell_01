fcef4cd4af5e99693ee5d3f69c5a0babf9629079
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-26 11:29:34 +0530

    shell

--- 01-array.sh
+++ 01-array.sh
@@ -6,3 +6,9 @@ echo "the 1st valu is ${my_array}"
 echo "the 2st valu is ${my_array[1]}"
 echo "the 2st valu is ${my_array[2]}"
 echo "the 1st valu is ${my_array[@]}"
+
+num=$1
+num1=$2
+
+sum=$(($num + $num1))
+echo $sum
