6a12b2ead73941082628a7732294eae67b309839
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-26 12:46:03 +0530

    shell

---
+++ 05-logs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+
+USER=$(id -u)
+SCRIPT_NAME=$( $0 | cut -d "." -f1 )
+TIME_STAMP=$($date +%F+%H+%M+%S)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
+
+
+USER=$(id -u)
+
+if [ $USER -eq 0 ]
+then 
+    echo "you are super user"
+else 
+    echo "you are not super user"
+    exit 1 # manually exiting 
+fi 
+
+VALIDATION_FUNCTION(){
+    if [ $1 -eq 0 ]
+    then
+        echo "installation is successfull $2"
+    else 
+        echo "installation is failed $2"
+        exit 1 
+    fi 
+}
+
+dnf install mysql -y &> LOG_FILE
+
+VALIDATION_FUNCTION $? "installing mysql"
+
+dnf install yum -y  &> LOG_FILE
+VALIDATION_FUNCTION $? "installing yum "
