6e2338ee2b58a15c66fae48468a1b81a0159adda
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-26 12:28:38 +0530

    shell

---
+++ 03-basic-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USER=(id -u)
+
+if [$USER -eq 0 ]
+then 
+    echo "you are super user"
+else 
+    echo "you are not super user"
+    exit 1 # manually exiting 
+fi 
+
+dnf install mysql -y 
+
