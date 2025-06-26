9bb5cfb6086f6b226b2c56674a2b721aba204fd1
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-06-26 11:24:08 +0530

    shell

--- 01-array.sh
+++ 01-array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-my_array=("rrr","tillu","hello")
+my_array=("rrr" "tillu" "hello")
 
 echo "the 1st valu is ${my_array[0]}"
 echo "the 2st valu is ${my_array[1]}"
 echo "the 3st valu is ${my_array[2]}"
 
-echo "all movies are ${my_array[@]}
+echo "all movies are ${my_array[*]}
